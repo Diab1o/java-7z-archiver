5a2aa879c0e0d949507f5d0c63ecbc9140f2ce29
Alexander Sokolov <dia.sokolov@gmail.com>
2015-07-04 02:00:50 +0300

    test commit

--- src/java/com/swemel/common/HRESULT.java
+++ src/java/com/swemel/common/HRESULT.java
@@ -12,6 +12,5 @@ public class HRESULT {
     public static final int S_FALSE = 1;
 
     public static final int E_NOTIMPL = 0x80004001;
-    public static final int E_FAIL = 0x80004005;
-    public static final int E_INVALIDARG = 0x80070057;
+
 }
