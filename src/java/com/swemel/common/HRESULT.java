5f6fa0d1702902bb9bc75dacdda70a35db482f0d
Stede Vane <StedeVane@GitHub.com>
2015-07-04 01:53:39 +0300

    test blah-blah-blah

--- src/java/com/swemel/common/HRESULT.java
+++ src/java/com/swemel/common/HRESULT.java
@@ -1,11 +1,7 @@
 package com.swemel.common;
 
 /**
- * Created by IntelliJ IDEA.
- * User: sokolov_a
- * Date: 10.02.2011
- * Time: 19:05:40
- * To change this template use File | Settings | File Templates.
+
  */
 public class HRESULT {
     public static final int S_OK = 0;
